d92435290a558f0e5d0cf7c5d66144e70cb393d7
Marco <marco_urena23@outlook.com>
2026-01-21 18:51:15 -0600

    decoratos, property

--- magicMethods.py
+++ magicMethods.py
@@ -19,6 +19,19 @@ class Book:
 	def __add__(self, other):
 		return self.num_pages + other.num_pages
 
+	def __contains__(self, keyword):
+		return keyword in self.title
+
+	def __getitem__(self, key):
+		if key in "title":
+			return self.title
+		elif key in "author":
+			return self.author
+		elif key in "num_pages":
+			return self.num_pages
+		else: 
+			return f"Key {key} was not found"
+
 book1 = Book("The Hobbit", "J.R.R Tolkien", 312)
 book2 = Book("Harry Potter and The Philosopher's Stone", "J.K Roling", 223)
 
@@ -27,3 +40,8 @@ print(book1 == book2)
 print(book1 < book2)
 print(book1 > book2)
 print(book1 + book2)
+print("Hobbit" in book1)
+print(book1['title'])
+print(book1['author'])
+print(book1['num_pages'])
+print(book1['audio'])
