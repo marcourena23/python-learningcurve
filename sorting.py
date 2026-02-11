39af143ceec48621f0b43d93faf66069a19bc36f
Marco <marco_urena23@outlook.com>
2026-02-10 22:12:07 -0600

    sorting.py

---
+++ sorting.py
@@ -0,0 +1,14 @@
+# Sorting in python .shot() or sorted()
+# Lists[], Tuples(), Dictionaries{"":""}, Objects
+
+# ----------------List--------------------
+fruits = ["banana", "orange", "apple", "coconut"]
+fruits.sort() # Order alphabetic
+print(fruits)
+
+numbers = [4,3,2,5,1]
+numbers.sort(reverse=True) # 5,4,3,2,1
+print(numbers)
+
+# --------------Tuples--------------------
+
