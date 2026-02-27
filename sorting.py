67c913b4f43ee51ee10772f113576959d72068bc
Marco <marco_urena23@outlook.com>
2026-02-26 22:11:59 -0600

    add zip and sorting files

--- sorting.py
+++ sorting.py
@@ -1,14 +1,54 @@
-# Sorting in python .shot() or sorted()
+# SORTING IN PYTHON .sort() or sorted()
 # Lists[], Tuples(), Dictionaries{"":""}, Objects
 
-# ----------------List--------------------
+#--------- LISTS ---------
 fruits = ["banana", "orange", "apple", "coconut"]
-fruits.sort() # Order alphabetic
+
+fruits.sort()
+fruits.sort(reverse=True)
+
 print(fruits)
 
-numbers = [4,3,2,5,1]
-numbers.sort(reverse=True) # 5,4,3,2,1
-print(numbers)
+#--------- TUPLES ---------
+fruits = ("banana", "orange", "apple", "coconut")
 
-# --------------Tuples--------------------
+fruits = tuple(sorted(fruits))
+fruits = tuple(sorted(fruits, reverse=True))
 
+print(fruits)
+
+#--------- DICTIONARIES ---------
+fruits = {
+   "banana": 105,
+   "apple": 72,
+   "orange": 73,
+   "coconut": 354
+}
+
+fruits = dict(sorted(fruits.items()))
+fruits = dict(sorted(fruits.items(), key=lambda item: item[0], reverse=True))
+fruits = dict(sorted(fruits.items(), key=lambda item: item[1]))
+fruits = dict(sorted(fruits.items(), key=lambda item: item[1], reverse=True))
+
+print(fruits)
+
+#--------- OBJECTS ---------
+class Fruit:
+   def __init__(self, name, calories):
+       self.name = name
+       self.calories = calories
+
+   def __repr__(self):
+       return f"{self.name}: {self.calories}"
+
+fruits = [Fruit("banana", 105),
+              Fruit("apple", 72),
+              Fruit("orange", 73),
+              Fruit("coconut", 354)]
+
+fruits = sorted(fruits, key=lambda fruit: fruit.name)
+fruits = sorted(fruits, key=lambda fruit: fruit.name, reverse=True)
+fruits = sorted(fruits, key=lambda fruit: fruit.calories)
+fruits = sorted(fruits, key=lambda fruit: fruit.calories, reverse=True)
+
+print(fruits)
--- zip.py
+++ zip.py
@@ -1,1 +1,12 @@
-# zip() = 
+# zip() = Combines multiple iterables (lists, tuples, sets, dict)
+#             into a single iterator of tuples.
+#             Makes managing multiple indices easier.
+
+names = ["Spongebob", "Patrick", "Squidward"]
+ages = [30, 35, 50]
+jobs = ["Cook", "Unemployed", "Cashier"]
+
+data = zip(names, ages, jobs)
+
+for name, age, job in data:
+    print(f"{name} is a {age} year old {job}")
