dffd452d4315160eee2e568c5e513ba1406f2db1
Marco <marco_urena23@outlook.com>
2026-06-07 19:44:31 -0600

    add recursive file

---
+++ recursion.py
@@ -0,0 +1,6 @@
+'''
+recursion = a function that calls itself from within
+            helps to visualiza a complex problem into basic steps,
+            wich can be solved more easily iteratively or recursively.
+'''
+
